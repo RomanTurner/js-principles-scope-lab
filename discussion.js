4fd492384febe52913eae2cca746b0fc63aaf1c1
Roman Turner <rn.turner@live.com>
2021-02-23 10:28:08 -0800

    assingment complete

---
+++ discussion.js
@@ -0,0 +1,42 @@
+
+const playMusic = music => ( "Playing some " + music)
+
+/** 
+ *
+ * 
+ * const playMusic = function (music) {
+  return "Playing some " + music
+}
+
+console.log(playMusic("Jazz"));
+playMusic("Jazz") // "Playing some Jazz" 
+const playMusic = (music) => {
+  return "Playing some " + music
+}
+
+playMusic("Jazz") // "Playing some Jazz"
+ 
+const playMusic = music => {
+  return "Playing some " + music
+}
+
+playMusic("Jazz") // "Playing some Jazz"
+ 
+const playMusic = music => {
+  "Playing some " + music
+}
+
+playMusic("Jazz") // undefined 
+const playMusic = music =>  "Playing some " + music
+playMusic("Jazz") // "Playing some Jazz" 
+
+const readBooks = (book1, book2) => {
+  return `I read '${book1}' and '${book2}'`
+}
+
+readBooks("The Old Man and the Sea", "1984") // "I read 'The Old Man and the Sea' and '1984'" 
+const readBooks = (book1, book2) =>  `I read '${book1}' and '${book2}'`
+readBooks("The Old Man and the Sea", "1984") // "I read 'The Old Man and the Sea' and '1984'" 
+const readBooks = book1, book2 =>  `I read '${book1}' and '${book2}'` // Syntax Error  
+
+*/
